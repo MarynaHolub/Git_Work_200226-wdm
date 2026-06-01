892c5ff1e4b28c6e8976f43b01418a266c61ece1
MarynaHolub <maryna.holub27@gmail.com>
2026-06-01 11:49:19 +0200

    Автоматический коммит 2026-06-01 11:49:19

--- autocommitMaryna.sh
+++ autocommitMaryna.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-#!/bin/bash
-
 if [ ! -d ".git" ]; then
-	echo "Это не git репозиторий"
-	exit 1
+    echo "Это не git репозиторий"
+    exit 1
 fi
 
 git add .
 
-if [ ! git diff-index --quit HEAD ]; then
-	echo "Нет изменений!"
-	exit 1
-	
-else 
-	git commit -m "Автоматический коммит $(date +'%Y-%m-%d %H:%M:%S')"
-	echo "успешном фиксировании изменений!"
+if git diff-index --quiet HEAD --; then
+    echo "Нет изменений!"
+    exit 0
+else
+    git commit -m "Автоматический коммит $(date +'%Y-%m-%d %H:%M:%S')"
+    echo "Изменения успешно зафиксированы!"
 fi
